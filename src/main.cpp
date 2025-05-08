65b90ec15eb9f2736cdeb2a14416babbe01c3e28
Your Name <omarelwkel17@gmail.com>
2025-05-08 22:55:32 +0300

    add generating processes function, print the processes details

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,49 @@
-#include<iostream>
+#include <iostream>
+#include <vector>
+#include <cstdlib>
+#include <ctime>
+#include <string>
 using namespace std;
 
+struct Process {
+    string ID;
+    int arrivalTime;
+    int priority;
+    int burstTime;
+};
+
+vector<Process> generateProcesses(int n = 5) {
+    vector<Process> processes;
+    int arrivalTime = 0;
+    srand(time(0));
+
+    for (int i = 1; i <= n; ++i) {
+        Process p;
+        p.ID = "P" + to_string(i);
+        if (arrivalTime < 5) {
+            arrivalTime += rand() % 3;
+            if (arrivalTime > 5) arrivalTime = 5;
+        }
+
+        p.arrivalTime = arrivalTime;
+        p.priority = rand() % 3 + 1;
+        p.burstTime = rand() % 5 + 1;
+
+        processes.push_back(p);
+    }
+
+    return processes;
+}
+
+
+void printProcesses(const vector<Process>& processes) {
+    cout << "P\tAr\tPr\tBu\n";
+    for (const auto& p : processes) {
+        cout << p.ID << "\t" << p.arrivalTime << "\t"
+             << p.priority << "\t" << p.burstTime << "\n";
+    }
+}
+
 int main() {
-    cout << "Welcome to Os project ^_^" << endl;
+    cout << "Welcome to the OS_Project ^_^";
 }
