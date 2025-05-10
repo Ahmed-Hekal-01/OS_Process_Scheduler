f148e0ed1aac0d83ed85c29905713ba3bccca4c4
Hekal <ahmedsanta021@gmail.com>
2025-05-10 12:43:17 +0300

    refactor add headerfile for Depndancies and fix return error

--- src/main.cpp
+++ src/main.cpp
@@ -1,67 +1,33 @@
-#include <iostream>
-#include <vector>
-#include <cstdlib>
-#include <ctime>
-#include <string>
-using namespace std;
+#include "schedulerServices.h"
 
-struct Process {
-    string ID;
-    int arrivalTime;
-    int priority;
-    int burstTime;
-    int time_remaining;
-};
-vector<Process> processes;
-vector<pair<string, int>> ganttChart;  // {ProcessID, EndTime}
-void GenerateProcesses(int n = 5) {
-    int arrivalTime = 0;
-    srand(time(0));
-
-    for (int i = 1; i <= n; ++i) {
-        Process p;
-        p.ID = "P" + to_string(i);
-        if (arrivalTime < 5) {
-            arrivalTime += rand() % 3;
-            if (arrivalTime > 5) arrivalTime = 5;
-        }
-
-        p.arrivalTime = arrivalTime;
-        p.priority = rand() % 3 + 1;
-        p.burstTime = rand() % 5 + 1;
-
-        processes.push_back(p);
-    }
-}
-vector<pair<string, int>> resolveRR(vector<Process>& group, int timeQuantum = 2 , int lastTimeStamp){
+vector<pair<string, int>> resolveRR(vector<Process>& group, int lastTimeStamp, int timeQuantum = 2){
     // Emad -> Implement here
+    return {};
 }
-vector<pair<string, int>> schedulePriorityRR(){
-    // hekal -> Implement here
+vector<pair<string, int>> schedulePriorityRR() {
+    return {};
 }
 double AvgWaitTime() {
     // Shaboury&Reem -> Implement here
+    return {};
 }
 double AvgTurnaroundTime() {
     // Shaboury&Reem -> Implement here
+    return{};
 }
 double AvgResponseTime() {
     // Shaboury&Reem -> Implement here
+    return{};
 }
 string GanttChart(const vector<pair<string, int>>& schedule) {
     // Omar_salah -> Implement here
-}
-void PintProcesses() {
-    cout << "P\tAr\tPr\tBu\n";
-    for (const auto& p : processes) {
-        cout << p.ID << "\t" << p.arrivalTime << "\t"
-             << p.priority << "\t" << p.burstTime << "\n";
-    }
+    return "";
 }
 void GUI() {
     // Shaboury&Reem -> Implement here
 }
 
 int main() {
+     schedulePriorityRR();
     cout << "Welcome to the OS_Project ^_^" << endl ;
 }
---
+++ src/schedulerServices.h
@@ -0,0 +1,49 @@
+//
+// Created by hekal on 5/10/25.
+//
+#ifndef SCHEDULERSERVICES_H
+#define SCHEDULERSERVICES_H
+#include <algorithm>
+#include <iostream>
+#include <vector>
+#include <cstdlib>
+#include <ctime>
+#include <string>
+#include <bits/ranges_algo.h>
+using namespace std;
+struct Process {
+    string ID;
+    int arrivalTime;
+    int priority;
+    int burstTime;
+    int time_remaining;
+};
+vector<Process> processes;
+vector<pair<string, int>> ganttChart;// {ProcessID, EndTime}
+void GenerateProcesses(int n = 5) {
+    int arrivalTime = 0;
+    srand(time(0));
+
+    for (int i = 1; i <= n; ++i) {
+        Process p;
+        p.ID = "P" + to_string(i);
+        if (arrivalTime < 5) {
+            arrivalTime += rand() % 3;
+            if (arrivalTime > 5) arrivalTime = 5;
+        }
+
+        p.arrivalTime = arrivalTime;
+        p.priority = rand() % 3 + 1;
+        p.burstTime = rand() % 5 + 1;
+
+        processes.push_back(p);
+    }
+}
+void PintProcesses() {
+    cout << "P\tAr\tPr\tBu\n";
+    for (const auto& p : processes) {
+        cout << p.ID << "\t" << p.arrivalTime << "\t"
+             << p.priority << "\t" << p.burstTime << "\n";
+    }
+}
+#endif //SCHEDULERSERVICES_H
